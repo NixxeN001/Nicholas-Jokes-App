e11076c2c420dac89ce90a446092060e600c0806
Nicholas Smith <80678073+NixxeN001@users.noreply.github.com>
2023-04-21 13:32:19 +0200

    setup the database complete. Can view the jokes database now

---
+++ Data/Migrations/20230421113100_jokeSetup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nicholas_Jokes_App.Data.Migrations
+{
+    public partial class jokeSetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "JokesContain",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    JokeAnswer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    JokeQuestion = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JokesContain", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JokesContain");
+        }
+    }
+}
