44b11f913498a10c2c14f167408eb03d8221ff94
Nicholas Smith <80678073+NixxeN001@users.noreply.github.com>
2023-04-21 13:12:14 +0200

    Setup the correction for the webpage

---
+++ Models/JokesContain.cs
@@ -0,0 +1,18 @@
+﻿namespace Nicholas_Jokes_App.Models
+{
+    public class JokesContain
+    {
+        public int JokeID { get; set; }
+
+        public string JokeAnswer { get; set; }
+
+        public string JokeQuestion { get; set; }
+
+        public JokesContain()
+        {
+            JokeQuestion = string.Empty;
+            JokeAnswer = string.Empty;
+        }
+
+    }
+}
